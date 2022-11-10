4f905bc8f1fe79a8add9aa1fbf7f00f07c200bd0
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2022-11-10 01:35:29 +0000

    collectibles, move, jump, sprint

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -6,10 +6,23 @@ public class CharacterController : MonoBehaviour
 {
     public float camRotationSpeed = -1.5f;
     public float rotationSpeed = 2.0f;
-    public float maxSpeed = 3.0f;
+    
+    public float maxSpeed;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
+    public float maxSprint = 5.0f;
+    float sprintTimer;
+
+
+    public float jumpForce = 300.0f;
 
     float rotation = 0.0f;
     float camRotation = 0.0f;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
 
     GameObject cam;
     Rigidbody myRigidbody;
@@ -19,13 +32,44 @@ public class CharacterController : MonoBehaviour
     {
         cam = GameObject.Find("Main Camera");
         myRigidbody = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        
+        sprintTimer = maxSprint;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
+
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        //When have half of legs
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);        
+        }
+
+        //when have all legs
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        } else
+        {
+            maxSpeed = normalSpeed;
+            if(Input.GetKey(KeyCode.LeftShift) == false)
+            {
+                sprintTimer = sprintTimer + Time.deltaTime;
+
+            }
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
      
 
