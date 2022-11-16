669a0a377caf5f4c1a3cab30aa9ae936ce8cc959
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2022-11-16 10:43:17 +0000

    moved pc

--- Assets/Script/MovingDoor.cs
+++ Assets/Script/MovingDoor.cs
@@ -5,18 +5,20 @@ using UnityEngine;
 public class MovingDoor : MonoBehaviour
 {
 
-
+    public Transform door;
+    public Transform startingPoint;
+    public Transform endingPoint;
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        door.position = startingPoint.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        door.position = Vector3.MoveTowards(door.position, endingPoint.position, Time.deltaTime);
     }
 }
