63b5d989e4e3f973c3785b12fafd0c46e1894aef
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2022-11-18 13:50:25 +0000

    variable

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    //camera
     public float camRotationSpeed = -1.5f;
     public float rotationSpeed = 2.0f;
-    
+   
+    //speed settings
     public float maxSpeed;
     public float normalSpeed = 10.0f;
     public float sprintSpeed = 20.0f;
-
+   
+    //sprint and jump settings
     public float maxSprint = 5.0f;
     float sprintTimer;
-
-
     public float jumpForce = 300.0f;
-
+    
+    
+    //camera settings
     float rotation = 0.0f;
     float camRotation = 0.0f;
 
@@ -36,36 +39,33 @@ public class CharacterController : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
         
         sprintTimer = maxSprint;
+
+        bool canSprint = false;
+        bool canClimb = false;
+        bool canJump = false;
+        bool canClimbFast = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        if (canJump == true)
+        {
+            jump();
+        }
         camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
+
+        if (canSprint == true)
+        {
+            sprint();
+        }
 
         isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
 
-        //When have half of legs
-        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            myRigidbody.AddForce(transform.up * jumpForce);        
-        }
+       
 
-        //when have all legs
-        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
-        {
-            maxSpeed = sprintSpeed;
-            sprintTimer = sprintTimer - Time.deltaTime;
-        } else
-        {
-            maxSpeed = normalSpeed;
-            if(Input.GetKey(KeyCode.LeftShift) == false)
-            {
-                sprintTimer = sprintTimer + Time.deltaTime;
-
-            }
-        }
+       
 
         sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
 
@@ -79,4 +79,33 @@ public class CharacterController : MonoBehaviour
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
     }
+    
+    private void jump()
+    {
+        //When have half of legs
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);        
+        }
+
+    }
+
+    private void sprint()
+    {
+         //when have all legs
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        } else
+        {
+            maxSpeed = normalSpeed;
+            if(Input.GetKey(KeyCode.LeftShift) == false)
+            {
+                sprintTimer = sprintTimer + Time.deltaTime;
+
+            }
+        }
+    }
+
 }
