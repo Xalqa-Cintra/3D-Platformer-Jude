0dd1d119570ac756e198d6a6b12618674c820bba
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2022-11-21 22:28:40 +0000

    work on bools

--- Assets/Script/BoneCollectionScript.cs
+++ Assets/Script/BoneCollectionScript.cs
@@ -24,7 +24,7 @@ public class BoneCollectionScript : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
 
-        if (GameObject.)
+        if (GameObject) ;
         pm.canClimb = true;
 
         
